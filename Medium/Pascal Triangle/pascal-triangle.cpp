141edb9f200012cd01121ae5a379099b5eae4e4b
Aayush Parajuli <99806454+aayush1693@users.noreply.github.com>
2024-06-03 04:36:40 -0700

    Added Solution - GfG to GitHub

---
+++ Medium/Pascal Triangle/pascal-triangle.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// #define ll long long
+class Solution{
+public:
+    vector<long long> nthRowOfPascalTriangle(int n) {
+        // code here
+    vector<vector<long long>> ans(n);
+    for(int i=0; i<n; i++){
+        ans[i].resize(i+1);
+        ans[i][0]=1;
+        ans[i][i]=1;
+        for(int j=1; j<i; j++){
+            ans[i][j]=(ans[i-1][j-1]+ ans[i-1][j]) % 1000000007;
+        }
+    }
+    return ans[n-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+void printAns(vector<long long> &ans) {
+    for (auto &x : ans) {
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.nthRowOfPascalTriangle(n);
+        printAns(ans);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
