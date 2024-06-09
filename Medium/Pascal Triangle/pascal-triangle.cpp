e8cfafa1a09390231dc8c2d14f578ea5364cd40c
Aayush Parajuli <99806454+aayush1693@users.noreply.github.com>
2024-06-09 08:34:37 +0545

    Updated Solution - GfG to GitHub

--- Medium/Pascal Triangle/pascal-triangle.cpp
+++ Medium/Pascal Triangle/pascal-triangle.cpp
@@ -8,16 +8,16 @@ class Solution{
 public:
     vector<long long> nthRowOfPascalTriangle(int n) {
         // code here
-    vector<vector<long long>> ans(n);
-    for(int i=0; i<n; i++){
-        ans[i].resize(i+1);
-        ans[i][0]=1;
-        ans[i][i]=1;
-        for(int j=1; j<i; j++){
-            ans[i][j]=(ans[i-1][j-1]+ ans[i-1][j]) % 1000000007;
+        vector<vector<long long>> ans(n);
+        for(int i=0; i<n; i++){
+            ans[i].resize(i+1);
+            ans[i][0]=1;
+            ans[i][i]=1;
+            for(int j=1; j<i; j++){
+                ans[i][j]=(ans[i-1][j-1]+ans[i-1][j])%1000000007;
+            }
         }
-    }
-    return ans[n-1];
+        return ans[n-1];
     }
 };
 
